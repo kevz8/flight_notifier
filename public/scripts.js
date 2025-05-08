71d7bd28516dedf9ed12944232ce48cf28675d1d
kevz8 <kevz8811@gmail.com>
2025-05-07 22:18:29 -0700

    Removed return date and added graphing for time-series and clearing the database

--- public/scripts.js
+++ public/scripts.js
@@ -2,12 +2,12 @@ async function retrieve() {
     let origin = document.getElementById("origin").value.trim();
     let destination = document.getElementById("destination").value.trim();
     let departDate = document.getElementById("departDate").value.trim(); 
-    let returnDate = document.getElementById("returnDate").value.trim();
+    // let returnDate = document.getElementById("returnDate").value.trim();
     let email = document.getElementById("email").value.trim(); 
 
-    if (returnDate === '') returnDate = null;
+    // if (returnDate === '') returnDate = null;
 
-    const data = {origin, destination, departDate, returnDate, email};
+    const data = {origin, destination, departDate, email};
 
     const resultSection = document.getElementById("result");
     resultSection.style.display = "block";
@@ -28,6 +28,32 @@ async function retrieve() {
     }
 }
 
+async function clear_db() {
+    const res = await fetch('/clear', {method: 'POST'});
+    const json = await res.json();
+    const resultSection = document.getElementById("result");
+    resultSection.style.display = "block";
+    resultSection.textContent = 'Successfully cleared';
+}
+
+async function graph() {
+    let origin = document.getElementById("origin").value.trim();
+    let destination = document.getElementById("destination").value.trim();
+    let departDate = document.getElementById("departDate").value.trim();
+
+    if (!origin || !destination || !departDate) {
+        const resultSection = document.getElementById("result");
+        resultSection.style.display = "block";
+        resultSection.textContent = 'Please fill in missing values';
+        return;
+    }
+
+    let params = new URLSearchParams({origin, destination, departDate});
+    window.open(`/graph?${params.toString()}`, '_blank');
+}
+
 window.onload = function() {
     document.getElementById('search').addEventListener('click', retrieve);
+    document.getElementById('graph').addEventListener('click', graph);
+    document.getElementById('clear').addEventListener('click', clear_db);
 };
